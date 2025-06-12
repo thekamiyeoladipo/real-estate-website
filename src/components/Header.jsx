327b26876c50ff4f281df4d446667d6f0742ec49
kamiye <kamiye31@gmail.com>
2025-06-12 20:31:57 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import Navbar from './Navbar'
+
+
+const Header = () => {
+  return (
+    <div className='min-h-screen w-full bg-center flex
+    items-center mb-4 overflow-hidden bg-cover bg-no-repeat'
+    style={{backgroundImage: `url(${assets.header_img})`}}
+    id='Header'>
+        <Navbar />
+        <div className='container text-center mx-auto py-4
+          px-6 md:px-20 lg:px-32 text-white'>
+            <h2 className='text-white text-6xl font-bold
+            sm:text-6xl md:text-[82px] inline-block max-w-3xl
+            pt-20'>
+                Find Your Dream Home
+            </h2>
+          <div className='space-x-4 mt-16 '>
+            <a href="#Projects" className='border border-white
+            px-8 py-3 rounded hover:bg-white hover:text-black
+            transition-all duration-300'>Projects</a>
+            <a href="#Contact" className='border border-white
+            px-8 py-3 rounded hover:bg-white hover:text-black
+            transition-all duration-300'>Contact us</a>
+          </div>
+        </div>
+    </div>
+  ) 
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { assets }from '../assets/assets'
+
+
+const Navbar = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full z-10 ">
+      <div
+        className="flex justify-between items-center container
+      mx-auto px-6 py-4 md:px-20 lg:px-32 bg-transparent"
+      >
+        <img src={assets.logo} alt="" />
+        <ul className="hidden md:flex gap-7 text-white">
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#About" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Services" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#Contact" className="cursor-pointer hover:text-gray-400">
+            Testimonials
+          </a>
+        </ul>
+        <button className="hidden md:block bg-white px-4 py-2 rounded-md">
+          Sign Up
+        </button>
+        <img className="md:hidden w-7 h-7" src={assets.menu_icon} alt="" />
+      </div>
+      {/*  mobile menu */}
+      <div className="
+      md:hidden fixed top-0 left-0 bottom-0 
+      overflow-hidden  w-full h-screen bg-white 
+      transition-all duration-300">
+        <div>
+          <img src={assets.cross_icon} alt="" />
+        </div>
+       
+        <ul className="flex flex-col items-center justify-center h-full">
+          <a
+            href="#Header"
+            className="
+          px-4 py-2 rounded-full inline-block
+          cursor-pointer hover:text-gray-400"
+          >
+            Home
+          </a>
+          <a
+            href="#About"
+            className="
+          px-4 py-2 rounded-full inline-block
+          cursor-pointer hover:text-gray-400"
+          >
+            About
+          </a>
+          <a
+            href="#Projects"
+            className="
+          px-4 py-2 rounded-full inline-block
+          cursor-pointer hover:text-gray-400"
+          >
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="
+          px-4 py-2 rounded-full inline-block
+          cursor-pointer hover:text-gray-400"
+          >
+            Testimonials
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar; 
+
