958bdaa331cf0ee8d897f2eadd2da2f15b6bf68a
kamiye <kamiye31@gmail.com>
2025-06-24 13:21:37 +0100

    increase max w of header hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
         <div className='container text-center mx-auto py-4
           px-6 md:px-20 lg:px-32 text-white'>
             <h2 className='text-white text-6xl font-bold
-            sm:text-6xl md:text-[82px] inline-block max-w-3xl
+            sm:text-6xl md:text-[82px] inline-block max-w-5xl
             pt-20' ref={headingRef}>
                 {"Find Your Dream Home".split("").map((char, index) => (
                   <span key={index} style={{display: 'inline-block'}}>
@@ -41,10 +41,10 @@ const Header = () => {
                 ))}
             </h2>
           <div className='space-x-4 mt-16 '>
-            <a href="#Projects" className='border border-white
+            <a href="#" className='border border-white
             px-8 py-3 rounded hover:bg-white hover:text-black
             transition-all duration-300'>Projects</a>
-            <a href="#Contact" className='border border-white
+            <a href="#" className='border border-white
             px-8 py-3 rounded hover:bg-white hover:text-black
             transition-all duration-300'>Contact us</a>
           </div>
