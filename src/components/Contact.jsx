37a682bb9afd6458487ce6d166c8b75f0d976dee
kamiye <kamiye31@gmail.com>
2025-06-26 21:31:54 +0100

    Add form elements for user contact information in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,6 +26,48 @@ const Contact = () => {
       >
         Ready to Make a Move? Let's Connect and Find Your Dream Property Together!
       </p>
+
+      <form action="" className='max-w-2xl mx-auto text-gray-600 pt-8'>
+        <div className='flex flex-wrap'>
+            <div className='w-full md:w-1/2 text-left mb-8 md:pl-4'>
+                Your Name
+                <input 
+                className='w-full border border-gray-300
+                py-3 px-4 mt-2' 
+                type="text"
+                 placeholder='Your name' name="Name" id="" 
+                 required
+                 stytle={{outline: 'none', border: 'none'}} 
+                 />
+                 
+            </div>
+
+             <div className='w-full md:w-1/2 text-left mb-8 md:pl-4'>
+                Your Email
+                <input 
+                className='w-full border border-gray-300
+                py-3 px-4 mt-2' 
+                type="text"
+                 placeholder='Your email' name="Email" id="" 
+                 required
+                 stytle={{outline: 'none', border: 'none'}} 
+                 />
+                 
+            </div>
+            
+        </div>
+
+        <div className='my-6 text-left'>
+            Message
+            <textarea className='w-full border border-gray-300
+            py-3 px-4 mt-2 h-48 resize-none'
+            name='Message' placeholder='Message' required></textarea>
+        </div>
+
+        <button className='rounded-md bg-blue-600 text-white py-2 px-12 mb-10'>
+            Send Mesage
+        </button>
+      </form>
     </div>
   );
 }
