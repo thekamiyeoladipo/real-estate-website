821f1d531c115076ddba607acd474e538d4eeabd
kamiye <kamiye31@gmail.com>
2025-06-26 21:10:04 +0100

    Implement Contact component with layout and content for user engagement

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <div
+      id="Contact"
+      className="text-center p-6 py-20 lg:px-32 w-full 
+    overflow-hidden"
+    >
+      <h1
+        className="text-2xl font-bold mb-2
+        sm:text-4xl text-center"
+      >
+        Get in Touch
+        <span
+          className="underline underline-offset-4
+          decoration-1 under font-light"
+        >
+          {" "}
+          With Us
+        </span>
+      </h1>
+      <p
+        className="text-center text-gray-500
+        mb-8 max-w-80 mx-auto"
+      >
+        Ready to Make a Move? Let's Connect and Find Your Dream Property Together!
+      </p>
+    </div>
+  );
 }
 
 export default Contact
